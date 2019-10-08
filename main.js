34a8dde48a3b20102d50213af4e379e445c85d6c
Adam Wilson <adamwilson1234@gmail.com>
2019-10-08 16:54:32 -0400

    oath auth and redirect worked without client js lib

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 /**
- * OPTIONS
+ * SETUP, KEYS, IDS ETC.
  */
-const CLIENT_ID =
-    "683892034585-dtge9o7j0k869dgo1hro0m77e42da6na.apps.googleusercontent.com";
+const YOUR_CLIENT_ID =
+	"683892034585-dtge9o7j0k869dgo1hro0m77e42da6na.apps.googleusercontent.com";
+const YOUR_REDIRECT_URI = "https://awilsongithub.github.io/youtube-client/";
 // rest api
 const DISCOVERY_DOCS = [
     "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
@@ -11,79 +12,41 @@ const YOUTUBE_API_KEY = "AIzaSyCLDvzBzRuXgmsA0GUFUpQNdtSG22gqSr4";
 // no write
 const SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"];
 const DEFAULT_CHANNEL = 'techguyweb';
-const authButton = document.getElementById("authorize-button");
-const signoutButton = document.getElementById("signout-button");
-const content = document.getElementById("content");
-const channelForm = document.getElementById("channel-form");
-const channelInput = document.getElementById("channel-input");
-const videoContainer = document.getElementById("video-container");
+// Request access from google oath2 here:
 
-/**
- * INIT
- * */
-document.addEventListener("DOMContentLoaded", () => {
-    // oauth name
-    // aw-youtube-clientid
-    // client id - ok to share
-    // "683892034585-dtge9o7j0k869dgo1hro0m77e42da6na.apps.googleusercontent.com"
-    //client secret - don't share
-    // "SjpOQHAy3yu1iwfJXc8X6Nck"
-});
+// response type
+// redirect_uri
 
-/**
- * FNS
- * */
+/*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+function oauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
-function handleClientLoad() {
-	gapi.load("client:auth2", initClient);
-	console.log(gapi)
-}
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
 
-// init api client lib with server/api url, scope, clientid
-// and setup sign in listeners
-function initClient() {
-    gapi.client
-        .init({
-            discoveryDocs: DISCOVERY_DOCS,
-            clientId: CLIENT_ID,
-            scope: SCOPES
-        })
-        .then(() => {
-			alert('in then')
-            // Listen for sign in state changes
-            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-            // Handle initial sign in state
-            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-            authButton.onclick = handleAuthClick;
-            signoutButton.onclick = handleSignoutClick;
-        });
-}
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': YOUR_CLIENT_ID,
+                'redirect_uri': YOUR_REDIRECT_URI,
+                'response_type': 'token',
+                'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
 
-// update ui sigin in state changes
-function updateSigninStatus(isSignedIn) {
-	alert('updating ui')
-    if (isSignedIn) {
-		alert('signed in');
-        authButton.style.display = "none";
-        signoutButton.style.display = "block";
-        content.style.display = "block";
-		videoContainer.style.display = "block";
-		getChannel(defaultChannel);
-    } else {
-		alert("NOT signed in");
-		authButton.style.display = "block";
-        signoutButton.style.display = "none";
-        content.style.display = "none";
-        videoContainer.style.display = "none";
-	}
-}
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
 
-// Handle login. call api auth sigin/sign out methods
-function handleAuthClick() {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-// Handle logout
-function handleSignoutClick() {
-  gapi.auth2.getAuthInstance().signOut();
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
 }
