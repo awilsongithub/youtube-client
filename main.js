c68fc54339f64991e33849c9819c2c5556c03ee4
Adam Wilson <adamwilson1234@gmail.com>
2019-10-09 12:34:37 -0400

    misc

--- main.js
+++ main.js
@@ -14,10 +14,59 @@ const DEFAULT_CHANNEL = "techguyweb";
 // redirect_uri
 
 /*===========================
+ * get hash of url when this page is requested
+ parse to see if from oauth 2 server (magic)
+ if token try api else start oauth2 flow
+ ===========================*/
+var fragmentString = location.hash.substring(1);
+
+// Parse query string to see if page request is coming from OAuth 2.0 server.
+var params = {};
+var regex = /([^&=]+)=([^&]*)/g,
+    m;
+while ((m = regex.exec(fragmentString))) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+}
+if (Object.keys(params).length > 0) {
+    localStorage.setItem("oauth2-test-params", JSON.stringify(params));
+    if (params["state"] && params["state"] == "try_sample_request") {
+        trySampleRequest();
+    }
+}
+
+// If there's an access token, try an API request.
+// Otherwise, start OAuth 2.0 flow.
+function trySampleRequest() {
+    var params = JSON.parse(localStorage.getItem("oauth2-test-params"));
+    if (params && params["access_token"]) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(
+            "GET",
+            "https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true&" +
+                "access_token=" +
+                params["access_token"]
+        );
+        xhr.onreadystatechange = function(e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.response);
+            } else if (xhr.readyState === 4 && xhr.status === 401) {
+                // Token invalid, so prompt for user permission.
+                oauthSignIn();
+            }
+        };
+        xhr.send(null);
+    } else {
+        oauthSignIn();
+    }
+}
+
+
+
+/*===========================
  * Create form to request 
  access token from 
  Google's OAuth 2.0 server.
- ===========================*/
+ ===========================*/d
 
 function oauthSignIn() {
     var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
